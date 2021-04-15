1a98c540ea502a85eb7f2fe64158ae8c4cb53ae2
Aron Horvath <horvatharon15@gmail.com>
2021-04-15 19:39:51 +0200

    fixed bugs

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID sOVEYFk7Q9llZVF56x8jhpJvCgIXuW_txQuNQ_0vFgs'
+        Authorization: 'Client-ID your-unsplash-api-key'
     }
 });
