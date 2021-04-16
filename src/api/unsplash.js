4a186fe52948cd38fbba757e267bfd6f24ca32ff
Aron Horvath <horvatharon15@gmail.com>
2021-04-16 11:23:02 +0200

    styling added

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID your-unsplash-api-key'
+        Authorization: 'Client-ID YOUR-UNSPLASH-APIKEY'
     }
 });
